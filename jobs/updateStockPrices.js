1e2e91033beef08f0651d3e1cc7f49320a0eafcb
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-06-28 10:53:27 +0530

    Create updateStockPrices.js

---
+++ jobs/updateStockPrices.js
@@ -0,0 +1,21 @@
+const cron = require('node-cron');
+const db = require('../models');
+const { Stock, PriceHistory } = db;
+
+cron.schedule('*/5 * * * *', async () => {
+  try {
+    const stocks = await Stock.findAll();
+    for (const stock of stocks) {
+      const newPrice = Math.floor(Math.random() * 100) + 1;
+      await PriceHistory.create({
+        stockId: stock.id,
+        price: newPrice
+      });
+      stock.price = newPrice;
+      await stock.save();
+    }
+    console.log(`[${new Date().toLocaleTimeString()}] Stock prices updated.`);
+  } catch (err) {
+    console.error('Price update failed:', err.message);
+  }
+});
